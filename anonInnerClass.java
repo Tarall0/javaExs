1c8bc7d694051af7fcae2f334fcadf83d806e8b2
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-06-28 16:12:55 +0200

    Update anonInnerClass.java

--- anonInnerClass.java
+++ anonInnerClass.java
@@ -9,7 +9,7 @@ public class De {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Test obj = new Test() {
+		Test obj = new Test() { // Inner class with overloaded method 
 			public void method() {
 				System.out.println("Anon Inner Class");
 			}
